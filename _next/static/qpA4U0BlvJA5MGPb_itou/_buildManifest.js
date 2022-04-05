a56909875f7bffffcbb022c8288c6c95f9cc08b5
timofeeva358456 <ayyyna.timofeeva@mail.ru>
2022-04-05 00:38:28 +0000

    Deploying to gh-pages from @ timofeeva358456/timofeeva358456.github.io@0d40d2dce7b20414d223fab0c1b2c456f4d24087 🚀

--- _next/static/qpA4U0BlvJA5MGPb_itou/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d9dcff8fd178bb8a.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],"/about":["static/chunks/pages/about-2665082ae1611364.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
